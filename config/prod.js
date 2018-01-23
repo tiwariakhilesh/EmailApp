38fbbfaee9208e6dc069a59b0b8584f4342ac11c
Akhilesh Tiwari <takhilesh178@gmail.com>
2018-01-23 13:48:09 +0530

    finished auth flow

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports={
+    googleClientId:process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongooseURL:process.env.MONGOOSE_URL,
+    cookiekeys:process.env.COOKIE_KEYS
+}
--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const mongoose= require('mongoose');
+const passport= require('passport');
+const cookieSession = require('cookie-session');
+const key = require('./config/keys');
+require('./models/user');
+require('./services/passport');
+
+mongoose.connect(key.mongooseURL);
 const app= express();
 
-app.get('/',(req, res) =>{
-    res.send({hi:'there'})
-});
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys:[key.cookiekeys]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
+require('./routes/authRoutes')(app);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+
+
+
+
